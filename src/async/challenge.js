a42f63d6868eda4513f8c5565d8f05710a392737
pikyr <pikytdv@gmail.com>
2022-07-14 11:37:05 -0300

    Fin de clase 14 - Try and catch

---
+++ src/async/challenge.js
@@ -0,0 +1,31 @@
+import fetch from "node-fetch";
+const API = 'https://api.escuelajs.co/api/v1';
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi);
+  const data = await response.json();
+
+  return data;
+}
+
+const otherFetchData = async (urlApi) => {
+  try {
+    const products = await fetchData(`${urlApi}/products`);
+
+    const productID = products[0].id;
+    const product = await fetchData(`${urlApi}/products/${productID}`);
+
+    const productCategoryID = product.category.id;
+    const category = await fetchData(`${urlApi}/categories/${productCategoryID}`);
+
+    console.log('Cantidad de producos: ', products.length);
+    console.log('Producto: ',product.title);
+    console.log('Categoria: ',category.name);
+    console.log('Objeto producto: ',product);
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+otherFetchData(API);
